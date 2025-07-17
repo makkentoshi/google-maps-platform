ad534617423ef15c9b645943857873f9827c1572
Mark <pochta.zdraste@mail.ru>
2025-07-18 02:49:05 +0500

    Update health check endpoint in index.js to return a test message and remove unused middleware

--- api/index.js
+++ api/index.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const cors = require('cors');
-
-
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: '10mb' }));
 
-// Health Check Endpoint
 app.get("/api/health", (req, res) => {
-
-  console.log("Safe mode health check was hit!");
-  res.status(200).json({ status: "SAFE MODE IS OK", time: new Date().toISOString() });
-});
-
-app.post('/api/recognize', async (req, res) => {
-
-    res.status(418).json({ message: "API is in safe mode. Problem is likely with ENV VARS." });
+  res.status(200).json({ 
+      status: "THIS IS THE TEST FROM 12:47, IT CANNOT FAIL" 
+  });
 });
 
 module.exports = app;
